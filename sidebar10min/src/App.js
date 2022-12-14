cada8ef9e6f8aada2f31d43831dc331d78d21b2d
krkopl <krko1973@gmail.com>
2022-12-14 18:05:09 +0100

    Podzielone na strony z pomoca Row/Col

--- sidebar10min/src/App.js
+++ sidebar10min/src/App.js
@@ -1,28 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-import "./App.css";
 import MySideNav from "./components/MySideNav";
-import Home from "./pages/Home";
 import Messages from "./pages/Messages";
 import Settings from "./pages/Settings";
 import Revenue from "./pages/Revenue";
 import NewUsers from "./pages/NewUsers";
+import Home from "./pages/Home";
+
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <MySideNav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/revenue" element={<Revenue />} />
-          <Route path="/new-users" element={<NewUsers />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Row>
+        <Col md="2">
+          <MySideNav />
+        </Col>
+        <Col>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/revenue" element={<Revenue />} />
+            <Route path="/new-users" element={<NewUsers />} />
+          </Routes>
+        </Col>
+      </Row>
+    </Router>
   );
 }
 
