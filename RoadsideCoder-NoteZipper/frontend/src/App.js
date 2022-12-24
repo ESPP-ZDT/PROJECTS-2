c62b544f2b27d9c1ad0d4d8520f427c1bd921a62
krkopl <krko1973@gmail.com>
2022-12-24 20:22:19 +0100

    simplified frontend added

---
+++ RoadsideCoder-NoteZipper/frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return <h1>Hello like before</h1>;
+}
+
+// alternative syntax
+// const App = () => { return <h1>Hello like before</h1>; };
+// const App = () => <h1>Hello like before</h1>;
+
+export default App;
