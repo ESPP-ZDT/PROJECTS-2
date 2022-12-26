beb71416efb571f9eb7d2883ff29ba1d607db943
krkopl <krko1973@gmail.com>
2022-12-26 10:16:37 +0100

    backend -> concurrently (why?)
    frontend -> axios, proxy for CORS, MyNotes with Accordion

--- RoadsideCoder-NoteZipper/frontend/src/screens/MyNotes/MyNotes.js
+++ RoadsideCoder-NoteZipper/frontend/src/screens/MyNotes/MyNotes.js
@@ -1,8 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 import MainScreen from "../../components/MainScreen";
 
 const MyNotes = () => {
-  return <MainScreen title="Welcome back KRKO">MyNotes</MainScreen>;
+  const NAME = "Witek";
+  const [notes, setNotes] = useState([]);
+
+  const deleteHandler = (noteId) => {
+    if (window.confirm("Are you sure to delete item?")) {
+      console.log(noteId);
+    }
+  };
+
+  const fetchNotes = async () => {
+    const { data } = await axios.get("/api/notes");
+    setNotes(data);
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
+    <MainScreen title={`Welcome back ${NAME}`}>
+      <Link to="createnote">
+        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+          Create New Note
+        </Button>
+      </Link>
+
+      {notes.map((note) => (
+        <Accordion defaultActiveKey={["0"]} key={note._id}>
+          <Accordion.Item eventkey="0">
+            <Card style={{ margin: 2 }} key={note._id}>
+              <Card.Header style={{ display: "flex" }}>
+                <span
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    flex: 1,
+                    cursor: "pointer",
+                    alignSelf: "center",
+                    fontSize: 18,
+                  }}
+                >
+                  <Accordion.Button as={Card.Text} variant="link">
+                    {note.title}
+                  </Accordion.Button>
+                </span>
+
+                <div>
+                  <Button href={`/note/${note.id}`}>Edit</Button>
+
+                  <Button
+                    variant="danger"
+                    className="mx-2"
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Card.Header>
+
+              <Accordion.Collapse>
+                <Card.Body>
+                  <h4>
+                    <Badge bg="success" text="light">
+                      Category - {note.category}{" "}
+                    </Badge>
+                  </h4>
+
+                  <blockquote className="blockquote mb-0">
+                    <p>{note.content}</p>
+
+                    <footer className="blockquote-footer">
+                      Creater on - date
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion.Item>
+        </Accordion>
+      ))}
+    </MainScreen>
+  );
 };
 
 export default MyNotes;
