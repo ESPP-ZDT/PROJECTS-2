473770f553607d0353cf8c023a55e1424c010aca
krkopl <krko1973@gmail.com>
2023-01-06 19:34:44 +0100

    useEffect --> auto redirect jeżeli zalogowany obiekt w localStorage

--- RoadsideCoder-NoteZipper/frontend/src/screens/LoginScreen/LoginScreen.js
+++ RoadsideCoder-NoteZipper/frontend/src/screens/LoginScreen/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 // import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -9,11 +10,19 @@ import ErrorMessage from "../../components/ErrorMessage";
 
 import "./LoginScreen.css";
 
-const LoginScreen = () => {
+const LoginScreen = ({ history }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem("userInfo");
+    if (userInfo) {
+      navigate("/mynotes");
+    }
+  }, [history]);
 
   const submitHandler = async (e) => {
     e.preventDefault();
